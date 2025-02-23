9a68e4626a556e3299d9d9ad530766814c7e2712
Wooseop Kim <wooseop@protonmail.com>
2025-02-23 20:00:27 +0900

    Add Nix shell

---
+++ shell.nix
@@ -0,0 +1,32 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+with pkgs;
+mkShell {
+  packages = [
+    bun
+    nodejs
+    act
+    nixfmt-rfc-style
+    nil
+  ];
+
+  PLAYWRIGHT_BROWSERS_PATH = playwright-driver.browsers;
+  PLAYWRIGHT_HOST_PLATFORM_OVERRIDE = "nixos";
+
+  shellHook =
+    let
+      playwrightPackage = x: "${x}@file:${playwright-test}/lib/node_modules/${x}";
+      playwrightPackages = builtins.map playwrightPackage [
+        "playwright"
+        "playwright-core"
+        "@playwright/test"
+      ];
+    in
+    ''
+      bun install --dev --no-save --exact \
+        ${lib.concatStringsSep " " playwrightPackages}
+      bun --version >.bun-version
+      node --version >.node-version
+    '';
+}
